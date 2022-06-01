4330cbef549dc3c20327232456dc69dbc1a81171
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-06-01 23:46:40 +0300

    feat Add behavioral patterns page

---
+++ iOS/Junior2/J2DesignPatterns.playground/Pages/BehavioralPatterns.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+/*:
+ [Previous](@previous)
+
+ ---
+
+ # Behavioral patterns (Поведенческие паттерны)
+
+ ---
+
+ - [Chain of Responsigiblity (Цепочка обязанностей)](ChainOfResponsibility)
+ - [Command (Команда)](Command)
+ - [Iterator (Итератор)](Iterator)
+ - [Mediator (Посредник)](Mediator)
+ - [Memento (Снимок)](Memento)
+ - [Observer (Наблюдатель)](Observer)
+ - [State (Состояние)](State)
+ - [Strategy (Стратегия)](Strategy)
+ - [Template method (Шаблонный метод)](TemplateMethod)
+ - [Visitor (Посетитель)](Visitor)
+ 
+ ---
+
+ [Next](@next)
+ */
